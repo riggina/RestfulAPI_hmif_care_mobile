30473d7a94f72af4c8c80cdc68ee1b5043fea43b
“Your <fikri.alichsan@gmail.com>
2022-12-07 00:00:49 +0800

    ping

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ const ReviewRoutes = require("./Review");
 
 const router = express.Router();
 
-router.get("/ping", (req,res) => {
+router.get("/ping"), (req,res) => {
     res.status(200).send("Welcome to HMIF CARE");
-
+}
 
 router.use("/user", UserRoutes);
 router.use("/quiz",auth, QuizRoutes);
